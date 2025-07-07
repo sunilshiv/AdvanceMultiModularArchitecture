4d255a51a76317286e70c81244ce5e398a6ae9a3
sunilshiv <sunil.d.shiv@gmail.com>
2025-07-07 23:18:46 +0530

    Add more dependencies and plugins to App build.gradle file and login module build.gradle file

--- buildSrc/src/main/kotlin/deps/Dependencies.kt
+++ buildSrc/src/main/kotlin/deps/Dependencies.kt
@@ -9,12 +9,21 @@ object Dependencies {
     const val ANDROIDX_ACTIVITY_COMPOSE =
         "androidx.activity:activity-compose:${DependenciesVersions.ACTIVITY_COMPOSE}"
     const val ANDROIDX_UI = "androidx.compose.ui:ui:${DependenciesVersions.COMPOSE_UI}"
-    const val ANDROIDX_UI_GRAPHICS = "androidx.compose.ui:ui-graphics:${DependenciesVersions.COMPOSE_UI}"
-    const val ANDROIDX_UI_TOOLING_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${DependenciesVersions.COMPOSE_UI}"
-  //  const val ANDROIDX_MATERIAL= "com.google.android.material:material:${deps.DependenciesVersions.MATERIAL_3}"
-    const val ANDROIDX_APP_COMPACT = "androidx.appcompat:appcompat:${DependenciesVersions.APP_COMPACT_VERSION}"
-    const val ANDROIDX_ACTIVITY_COMPOSE_BOM = "androidx.compose:compose-bom:${DependenciesVersions.ACTIVITY_COMPOSE_BOM}"
-    const val ANDROIDX_ACTIVITY = "androidx.activity:activity:${DependenciesVersions.ACTIVITY_COMPOSE}"
-    const val ANDROIDX_CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:${DependenciesVersions.ACTIVITY_CONSTRAINTLAYOUT}"
-   const val ANDROIDX_MATERIAL3= "androidx.compose.material3:material3:${DependenciesVersions.Android_MATERIAL_3}"
+    const val ANDROIDX_UI_GRAPHICS =
+        "androidx.compose.ui:ui-graphics:${DependenciesVersions.COMPOSE_UI}"
+    const val ANDROIDX_UI_TOOLING_PREVIEW =
+        "androidx.compose.ui:ui-tooling-preview:${DependenciesVersions.COMPOSE_UI}"
+
+    //  const val ANDROIDX_MATERIAL= "com.google.android.material:material:${deps.DependenciesVersions.MATERIAL_3}"
+    const val ANDROIDX_APP_COMPACT =
+        "androidx.appcompat:appcompat:${DependenciesVersions.APP_COMPACT_VERSION}"
+    const val ANDROIDX_ACTIVITY_COMPOSE_BOM =
+        "androidx.compose:compose-bom:${DependenciesVersions.ACTIVITY_COMPOSE_BOM}"
+    const val ANDROIDX_ACTIVITY =
+        "androidx.activity:activity:${DependenciesVersions.ACTIVITY_COMPOSE}"
+    const val ANDROIDX_CONSTRAINTLAYOUT =
+        "androidx.constraintlayout:constraintlayout:${DependenciesVersions.ACTIVITY_CONSTRAINTLAYOUT}"
+    const val ANDROIDX_MATERIAL3 =
+        "androidx.compose.material3:material3:${DependenciesVersions.Android_MATERIAL_3}"
+
 }
--- buildSrc/src/main/kotlin/deps/DependenciesVersions.kt
+++ buildSrc/src/main/kotlin/deps/DependenciesVersions.kt
@@ -16,4 +16,10 @@ object DependenciesVersions {
     const val ACTIVITY_CONSTRAINTLAYOUT = "2.1.4"
     const val Android_MATERIAL_3 = "1.2.1"
 
+    const val RETROFIT = "2.11.0"
+    const val RETROFIT_COROUTINE_ADAPTER_VERSION = "0.9.2"
+    const val OKHTTP = "5.0.0-alpha.2"
+    const val HILT = "2.44"
+    const val ROOM = "2.6.1"
+
 }
