a27157a9982b49a40bb3c9ea6e6711d4cb19073e
sunilshiv <sunil.d.shiv@gmail.com>
2025-07-30 11:29:05 +0530

    Update detekt plugin to the application level

--- app/src/main/java/com/demo/advancemultimodulararchitecture/ui/theme/Theme.kt
+++ app/src/main/java/com/demo/advancemultimodulararchitecture/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme =
   )
 
 @Composable
-fun AdvanceMultiModularArchitectureTheme(
+fun AdvancedMultiModularArchitectureTheme(
   darkTheme: Boolean = isSystemInDarkTheme(),
   // Dynamic color is available on Android 12+
   dynamicColor: Boolean = true,
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation("com.android.tools.build:gradle:8.7.3")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:7.2.1")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.3")
 }
